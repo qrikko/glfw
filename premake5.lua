332ae27da4f088deba4113d574f8a88483785587
Qrikko <qrikko@gmail.com>
2022-02-04 01:09:03 +0100

    something went bad during update almost fixed

--- premake5.lua
+++ premake5.lua
@@ -11,8 +11,7 @@ project "glfw"
 		"include/GLFW/glfw3.h",
 		"include/GLFW/glfw3native.h",
 		"src/glfw_config.h",
---		"src/context.c",
-		"src/glfw.rc.in",
+		"src/context.c",
 		"src/init.c",
 		"src/input.c",
 		"src/monitor.c",
@@ -33,14 +32,23 @@ project "glfw"
 		buildoptions { "-fPIC" }
 
 		files {
+			"src/egl_context.*",
+			"src/glfw.*",
+			"src/init.*",
+			"src/internal.*",
+			"src/platform.*",
+			"src/posix_module.*",
+			"src/null_*",
+
 			"src/x11_init.c",
 			"src/linux_joystick.c",
 			"src/x11_monitor.c",
 			"src/posix_time.c",
 			"src/posix_thread.c",
 			"src/x11_window.c",
+			"src/x11_platform.h",
 			"src/glx_context.c",
-			"src/egl_context.c",
+		--	"src/egl_context.c",
 			"src/osmesa_context.c",
 			"src/xkb_unicode.c"
 		}
